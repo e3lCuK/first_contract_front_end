d57713af6e6335676bdb408192c432cdd446e719
KotBegemot <kopitsa.egor.2020@gmail.com>
2024-09-22 17:19:51 +0300

    Not TWA Sdk

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import "./App.css";
 import { TonConnectButton } from "@tonconnect/ui-react";
 import { useMainContract } from "./hooks/useMainContract";
 import { useTonConnect } from "./hooks/useTonConnect";
-import WebApp from "@twa-dev/sdk";
+// import WebApp from "@twa-dev/sdk";
 
-WebApp.showAlert("Hey there!")
+// WebApp.showAlert("Hey there!")
 
 function App() {
   const {
@@ -20,9 +20,9 @@ function App() {
 
   const { connected } = useTonConnect()
 
-  const showAlert = () => {
-    WebApp.showAlert("Hey there!");
-  };
+  // const showAlert = () => {
+  //   WebApp.showAlert("Hey there!");
+  // };
 
   return (
     <div>
@@ -31,7 +31,7 @@ function App() {
       </div>
       <div>
         <div className='Card'>
-          <b>{WebApp.platform}</b>
+          {/* <b>{WebApp.platform}</b> */}
           <b>Наш контракт Адрес</b>
           <div className='Hint'>{contract_address?.slice(0, 30) + "..."}</div>
           <b>Наш контракт Баланс</b>
@@ -75,7 +75,7 @@ function App() {
           </a>
         )}
 
-        <br/>
+        {/* <br/>
         {connected && (
           <a
             onClick={() => {
@@ -84,7 +84,7 @@ function App() {
           >
             Показать предупреждение
           </a>
-        )}
+        )} */}
 
       </div>
     </div>
